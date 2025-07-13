8ca499e1ded3d8033f25c2962cd65f640e449051
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-13 20:38:13 +0200

    feat inizio esercizio

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const countdownEl = document.getElementById('countdown');
+    const instructionsEl = document.getElementById('instructions');
+    const numbersListEl = document.getElementById('numbers-list');
+    const answersForm = document.getElementById('answers-form');
+    const messageEl = document.getElementById('message');
+    const inputGroup = document.getElementById('input-group');
+
+    let randomNumbers = [];
+    const totalTime = 30;
+    const hideNumbersTime = 10;
+    let timeLeft = totalTime;
+
+    });
